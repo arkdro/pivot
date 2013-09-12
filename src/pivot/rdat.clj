bfe3a7c1d0adcddd0d44df787c35948f04289ca6
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-13 00:50:10 +0400

    add parse-data-line, parse-index-line

--- src/pivot/rdat.clj
+++ src/pivot/rdat.clj
@@ -5,16 +5,17 @@
 
 ;; (trace-ns 'pivot.other)
 
-(def regex #"(\d+)\s+(\d+)")
+(def regex #"\s+")
 
-(defn parse-line [line]
-  (let [[_ s1 s2] (re-find regex line)
-        data (filter #(not (nil? %)) [s1 s2])
-        [d1 d2] (map #(Integer. %) data)]
-    [d1 d2]))
+(defn parse-line-with-fun [f line]
+  (let [str-seq (clojure.string/split line regex)]
+    (map f str-seq)))
 
-(defn parse-size-line [size-line]
-  (parse-line size-line))
+(defn parse-index-line [line]
+  (parse-line-with-fun #(Integer. %) line))
+
+(defn parse-data-line [line]
+  (parse-line-with-fun #(Double. %) line))
 
 (defn valid-item [[n1 n2]]
   (and
@@ -22,14 +23,14 @@
    (not (= n2 nil))))
 
 (defn parse-data-lines [lines]
-  (filter valid-item (map parse-line lines)))
+  (filter valid-item (map parse-data-line lines)))
 
 (defn get-data [fname]
   (let [text (slurp fname)
         lines (clojure.string/split-lines text)
         size-line (first lines)
         data-lines (rest lines)
-        [n-items capacity] (parse-size-line size-line)
+        [n-items capacity] (parse-index-line size-line)
         items (vec (parse-data-lines data-lines))
         cnt-lines (count data-lines)
         cnt-items (count items)
