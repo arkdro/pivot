cec0fd154eafb77db3acb1c88369eabee3d9ae31
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-14 23:52:41 +0400

    add tests

--- test/pivot/test/rdat.clj
+++ test/pivot/test/rdat.clj
@@ -17,12 +17,34 @@
     (is (= [3.0 4.0 7.001] act))
     ))
 
-(def lines
+(def text
   "3 4\n1 3 6\n2 4 5 7\n1 3 0\n0 0 -1 -2\n1 -1 0 -1\n-1 0.001 -2 0\n1 -1  2 3 1\n\n\n"
   )
 
+(def lines ["3 4"
+            "1 3 6"
+            "2 4 5 7"
+            "1 3 0"
+            "0 0 -1 -2"
+            "1 -1 0 -1"
+            "-1 0.001 -2 0"
+            "1 -1  2 3 1"])
+
+(def parsed {
+             :m 3
+             :n 4
+             :basic-indexes [1 3 6]
+             :nonbasic-indexes [2 4 5 7]
+             :basic-values [1.0 3.0 0.0]
+             :matrix [[0.0 0.0 -1.0 -2.0]
+                      [1.0 -1.0 0.0 -1.0]
+                      [-1.0 0.001 -2.0 0.0]]
+             :obj-values [1.0 -1.0  2.0 3.0 1.0]
+             }
+  )
+
 (deftest split-text-to-lines-test
-  (let [act (pivot.rdat/split-text-to-lines lines)
+  (let [act (pivot.rdat/split-text-to-lines text)
         exp ["3 4"
              "1 3 6"
              "2 4 5 7"
@@ -31,6 +53,12 @@
              "1 -1 0 -1"
              "-1 0.001 -2 0"
              "1 -1  2 3 1"]]
+    (is (= exp act))
+    ))
+
+(deftest parse-all-lines-test
+  (let [act (pivot.rdat/parse-all-lines lines)
+        exp parsed]
     (is (= exp act))
     ))
 
