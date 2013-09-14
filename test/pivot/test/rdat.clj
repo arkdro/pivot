ad7cc9ec5db3abb2ccf85da5086034794b6308c0
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-14 22:45:31 +0400

    add test

--- test/pivot/test/rdat.clj
+++ test/pivot/test/rdat.clj
@@ -17,3 +17,20 @@
     (is (= [3.0 4.0 7.001] act))
     ))
 
+(def lines
+  "3 4\n1 3 6\n2 4 5 7\n1 3 0\n0 0 -1 -2\n1 -1 0 -1\n-1 0.001 -2 0\n1 -1  2 3 1\n\n\n"
+  )
+
+(deftest split-text-to-lines-test
+  (let [act (pivot.rdat/split-text-to-lines lines)
+        exp ["3 4"
+             "1 3 6"
+             "2 4 5 7"
+             "1 3 0"
+             "0 0 -1 -2"
+             "1 -1 0 -1"
+             "-1 0.001 -2 0"
+             "1 -1  2 3 1"]]
+    (is (= exp act))
+    ))
+
