4d72c587aee507b414e3c5c94db668d1e1ea99bc
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-15 16:08:38 +0400

    set mode for vim

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+;; -*- mode: clojure -*-
+;; ex: ts=4 sw=4 ft=lisp et
 (defproject pivot "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :main pivot.core
@@ -5,4 +7,5 @@
     [org.clojure/clojure "1.3.0"]
     [org.clojure/tools.cli "0.2.2"]
     [org.clojure/tools.trace "0.7.5"]
+    ;;[org.clojure/math.numeric-tower "0.0.2"]
   ])
