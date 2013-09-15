aa83a70833267733f5f8a554fe83e47f9466ab9d
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-15 16:09:41 +0400

    add min-by-ratio-or-index

--- src/pivot/step.clj
+++ src/pivot/step.clj
@@ -10,6 +10,22 @@
                    (if (empty? pos) nil
                        (reduce #(min-key first %1 %2) pos)))))
 
+(defn min-by-ratio-or-index
+  ([[_i1 _b1 _v1 :as item]]
+     item)
+  ([[i1 b1 v1 :as item1] [i2 b2 v2 :as item2]]
+     (cond
+       (and (= v1 0) (= v2 0)) nil
+       (= v1 0) item2
+       (= v2 0) item1
+       :default (let [r1 (m/abs (/ b1 v1))
+                      r2 (m/abs (/ b2 v2))]
+                  (cond
+                    (< r1 r2) item1
+                    (> r1 r2) item2
+                    (< i1 i2) item1
+                    :default item2)))))
+
 ;; {:m m
 ;;  :n n
 ;;  :basic-indexes basic-indexes
