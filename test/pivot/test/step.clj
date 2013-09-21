fbc46a1f07d5de4157570aefdaa7a056f95d0e06
Arkadiy Drovosekov <arkdro@gmail.com>
2013-09-21 21:05:43 +0400

    update tests

--- test/pivot/test/step.clj
+++ test/pivot/test/step.clj
@@ -6,11 +6,11 @@
 ;; (trace-ns 'pivot.step)
 
 (deftest find-entering-var-test
-  (is (= [3 2.1] (pivot.step/find-entering-var [3 1 2] [2.1 0 0])))
-  (is (= nil (pivot.step/find-entering-var [3 1 2] [0 0 0])))
-  (is (= [1 0.014] (pivot.step/find-entering-var [3 1 2] [2 0.014 11])))
-  (is (= [2 0.001] (pivot.step/find-entering-var [3 1 2] [-2 -1 0.001])))
-  (is (= nil (pivot.step/find-entering-var [3 1 2] [-2 -1 -0.001])))
+  (is (= [3 2.1] (pivot.step/find-entering-var [3 1 2] [nil 0 0 2.1])))
+  (is (= nil (pivot.step/find-entering-var [3 1 2] [nil 0 0 0])))
+  (is (= [1 0.014] (pivot.step/find-entering-var [3 1 2] [nil 0.014 11 2])))
+  (is (= [2 0.001] (pivot.step/find-entering-var [3 1 2] [nil -1 0.001 -2])))
+  (is (= nil (pivot.step/find-entering-var [3 1 2] [nil -1 -0.001 -2])))
   (is (= nil (pivot.step/find-entering-var [] [])))
   )
 
